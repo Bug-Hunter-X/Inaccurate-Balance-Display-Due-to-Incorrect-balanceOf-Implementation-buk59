733947702afc01150c8e363e436b2b0864d6c7cf
BugHunter-X <askzenai@gmail.com>
2025-01-09 17:34:05 +0100

    Add balanceOfSolution.sol file

---
+++ balanceOfSolution.sol
@@ -0,0 +1,21 @@
+function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+    
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        unchecked {
+            _balances[sender] = senderBalance - amount;
+        }
+        _balances[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+
+        _afterTokenTransfer(sender, recipient, amount);
+    } 
